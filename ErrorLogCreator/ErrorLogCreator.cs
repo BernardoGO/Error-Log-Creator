d11ed64ebb214152278ee6d63c610323ed5aae50
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-09-10 16:26:51 -0300

    init

---
+++ ErrorLogCreator/ErrorLogCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ErrorLogCreator
+{
+    public class ErrorLogCreator
+    {
+        public static string LogName = "ErrorLog.txt";
+        public static string AssemblyTitle = "Title";
+        public static string AssemblyVersion = "Version";
+        public static string AssemblyVersionInfo =  "VersionInfo";
+        public static void AddError(string Error)
+        {
+            StreamWriter writer = File.AppendText(LogName);
+            writer.WriteLine("-> " + AssemblyTitle + " v" + AssemblyVersion + AssemblyVersionInfo + " :: " + DateTime.Now.ToString() + " - Error: " + Error + "; <-");
+            writer.Close();
+        }
+    }
+}
