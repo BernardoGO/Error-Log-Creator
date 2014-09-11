c2821fd159b54c8c62a7187ee59ab5805843f260
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2014-09-11 16:27:44 -0300

    first

---
+++ Teste/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using CPFData;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpf = Console.ReadLine();
+            Console.WriteLine( clsCPFData.isCPF_Mod11(cpf).ToString()
+                );
+            Console.ReadKey();
+        }
+    }
+}
